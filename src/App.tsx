b6c01ad076cfdd134387ba2f04112beb41a24b78
Erica De Guzman <erica.deguzman@cbtravel.com>
2023-04-06 21:39:48 -0700

    mui calendar added.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Box, Typography } from '@mui/material'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DateCalendar, DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,10 @@ function App() {
   return (
     <Box>
       <Typography variant="h1">Hello!!</Typography>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar />
+      <DatePicker label="Basic date picker" />
+    </LocalizationProvider>
     </Box>
   )
 }
